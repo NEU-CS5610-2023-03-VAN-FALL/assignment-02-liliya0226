b6ed6cb444112b675830fb842c2e22b67f17029a
李明曦 <limingxi@limingxis-MacBook-Air.local>
2023-11-09 02:13:21 -0800

    upload

---
+++ api/index.js
@@ -0,0 +1,156 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+const prisma = new PrismaClient();
+
+// Create a new movie review
+app.post("/movie-review", async (req, res) => {
+  const { title, review } = req.body;
+
+  const maxReviewLength = 200;
+  if (review.length > maxReviewLength) {
+    return res.status(400).json({ error: "Review length exceeds the maximum limit" });
+  }
+
+  try {
+    const movieReview = await prisma.Movie.create({
+      data: {
+        title,
+        review,
+      },
+    });
+
+    res.json(movieReview);
+  } catch (error) {
+    console.error("Error creating movie review:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Get all movie reviews
+app.get("/movie-reviews", async (req, res) => {
+    try {
+      const movieReviews = await prisma.Movie.findMany({});
+  
+      // Backend data validation example
+      const validatedReviews = movieReviews.map((review) => {
+        if (!review.title || typeof review.title !== 'string') {
+          console.error(`Invalid data for review with ID ${review.id}: Missing or invalid title.`);
+          return null; // Skip sending this review to the client
+        }
+  
+        return {
+          id: review.id,
+          title: review.title,
+          // Add other properties to validate if needed
+        };
+      }).filter(Boolean); // Remove null values from the array
+  
+      res.json(validatedReviews);
+    } catch (error) {
+      console.error("Error fetching movie reviews:", error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+// Get a specific movie review by ID
+app.get("/movie-review/:id", async (req, res) => {
+    try {
+      const id = parseInt(req.params.id);
+  
+      if (isNaN(id) || id <= 0) {
+        return res.status(400).json({ error: "Invalid ID parameter" });
+      }
+  
+      const movieReview = await prisma.Movie.findUnique({
+        where: { id },
+      });
+  
+      if (!movieReview) {
+        return res.status(404).json({ error: "Movie review not found" });
+      }
+  
+      res.json(movieReview);
+    } catch (error) {
+      console.error("Error fetching movie review:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
+
+// Update a movie review
+app.put("/movie-review/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, review } = req.body;
+  
+    // Validate input
+    if (!title || !review) {
+      return res.status(400).json({ error: "Title and review are required" });
+    }
+  
+    // Check if the movie review exists
+    const existingReview = await prisma.Movie.findUnique({
+      where: { id },
+    });
+  
+    if (!existingReview) {
+      return res.status(404).json({ error: "Movie review not found" });
+    }
+
+    const maxReviewLength = 200;
+    if (review.length > maxReviewLength) {
+      return res.status(400).json({ error: "Review length exceeds the maximum limit" });
+    }
+
+    // Update the movie review
+    const updatedReview = await prisma.Movie.update({
+      where: { id },
+      data: {
+        title,
+        review,
+      },
+    });
+  
+    res.json(updatedReview);
+  });
+  
+
+// Delete a movie review
+app.delete("/movie-review/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    try {
+      // Check if the movie review exists before attempting to delete
+      const existingReview = await prisma.Movie.findUnique({
+        where: { id },
+      });
+  
+      if (!existingReview) {
+        return res.status(404).json({ error: "Movie review not found" });
+      }
+  
+      // If the review exists, proceed with the deletion
+      await prisma.Movie.delete({
+        where: { id },
+      });
+  
+      res.json({ message: "Movie review deleted" });
+    } catch (error) {
+      console.error("Error deleting movie review:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
+
+app.listen(8000, () => {
+  console.log("Server running on http://localhost:8000 🎉 🚀");
+});
